2e048594f9ef89d7747a7a0d4961976641c7771c
dmechea <daniel.mechea@gmail.com>
2018-12-31 13:48:11 +0900

    have solution to convert stuff category labels to panoptic category labels

--- classConversionDict.py
+++ classConversionDict.py
@@ -1,17 +1,11 @@
 # stuffID => stuffName => PanopticName => PanopticID => PanopticIndex
+# Do i need the panopticID? Maybe not
 
 import json
 from utils import loadJson, loadStuffTextLabels
 
 path_to_panoptic_classes = './dependencies/panopticapi/panoptic_coco_categories.json'
 path_to_stuff_classes = './dependencies/deeplab-pytorch/data/datasets/cocostuff/labels_2.txt'
-
-
-# In goes labels, out comes dictionary
-def createStuffToPanopticClassDictionary(stuff_class_dict, panoptic_class_dict):
-    print (stuff_class_dict)
-    print (panoptic_class_dict)
-
 
 # Panoptic categories is a list and I need to make a dictionary
 # That ties the id to position in list
@@ -22,18 +16,57 @@ def panopticToIndexPosition(panoptic_category_json):
         resulting_dict[id] = index
     return resulting_dict
 
+def makePanopticIdIndex(panoptic_category_list):
+    panopticClassDict = {}
+    for category in panoptic_category_list:
+        name = category['name']
+        id = category['id']
+        panopticClassDict[str(id)] = name
+    return panopticClassDict
 
 stuff_category_dict = loadStuffTextLabels(path_to_stuff_classes)
-panoptic_category_dict = loadJson(path_to_panoptic_classes)
+panoptic_category_list = loadJson(path_to_panoptic_classes)
 
-def convertStuffIDToPanopticIndex(id):
+
+def getAllMergedClasses(panoptic_category_dict):
+    mergedClasses = {}
+    for category in panoptic_category_dict:
+        name = category['name']
+        id = category['id']
+        removedMergedName = name.split('-')[0]
+        if 'merged' in name:
+            mergedClasses[removedMergedName] = id
+    return mergedClasses
+
+
+# print (panDict)
+
+# I want this to return the correct class Number
+def convertStuffIDToPanopticIndex(id, panoptic_category_index, stuff_category_index, merged_dict):
     strId = str(id) if not isinstance(id, str) else id
-    stuff_id_name = stuff_category_dict[strId]
-    print (stuff_id_name)
+    stuff_id_name = stuff_category_index.get(strId)
+    panoptic_id_name = panoptic_category_index.get(strId)
+
+    if stuff_id_name == panoptic_id_name:
+        return strId
+    else:
+        mergedDictResult = merged_dict.get(stuff_id_name)
+        if mergedDictResult is not None:
+            return str(mergedDictResult)
+        return mergedDictResult
+
 
 def stuffIdToPanopticIndex(stuff_id_dict, panoptic_index_dict):
     pass
 
-convertStuffIDToPanopticIndex(97)
+pan_idx = makePanopticIdIndex(panoptic_category_list)
+
+merged_dict = getAllMergedClasses(panoptic_category_list)
+
+# print (pan_idx)
+for i in stuff_category_dict:
+    id = convertStuffIDToPanopticIndex(i, pan_idx, stuff_category_dict, merged_dict)
+
+# convertStuffIDToPanopticIndex()
 
 # panopticToIndexPosition(panoptic_category_json)
--- utils.py
+++ utils.py
@@ -4,10 +4,13 @@ def loadJson(relative_file_location):
     with open(relative_file_location) as json_data:
         return json.load(json_data)
 
+def addOneToString(stringInt):
+    return str(int(stringInt) + 1)
+
 def loadStuffTextLabels(relative_file_location):
     file = open(relative_file_location, 'r')
     stuffLabels = {}
     for line in file:
         id, name = line.strip().split('\t')
-        stuffLabels[id] = name
+        stuffLabels[addOneToString(id)] = name
     return stuffLabels
