9f9e0c0b498e0edcab20c882113ab1cbea25552e
dmechea <daniel.mechea@gmail.com>
2018-12-31 11:07:01 +0900

    load up stuff labels, build conversion function

--- classConversionDict.py
+++ classConversionDict.py
@@ -1,18 +1,17 @@
-# Needs to be done by hand
-
-# conversionDictionary = {
-#     '97': '188', # cabinet
-# }
+# stuffID => stuffName => PanopticName => PanopticID => PanopticIndex
 
 import json
-from utils import loadJson
+from utils import loadJson, loadStuffTextLabels
 
 path_to_panoptic_classes = './dependencies/panopticapi/panoptic_coco_categories.json'
+path_to_stuff_classes = './dependencies/deeplab-pytorch/data/datasets/cocostuff/labels_2.txt'
+
 
 # In goes labels, out comes dictionary
 def createStuffToPanopticClassDictionary(stuff_class_dict, panoptic_class_dict):
     print (stuff_class_dict)
     print (panoptic_class_dict)
+
 
 # Panoptic categories is a list and I need to make a dictionary
 # That ties the id to position in list
@@ -23,6 +22,18 @@ def panopticToIndexPosition(panoptic_category_json):
         resulting_dict[id] = index
     return resulting_dict
 
-# panoptic_category_json = loadJson(path_to_panoptic_classes)
+
+stuff_category_dict = loadStuffTextLabels(path_to_stuff_classes)
+panoptic_category_dict = loadJson(path_to_panoptic_classes)
+
+def convertStuffIDToPanopticIndex(id):
+    strId = str(id) if not isinstance(id, str) else id
+    stuff_id_name = stuff_category_dict[strId]
+    print (stuff_id_name)
+
+def stuffIdToPanopticIndex(stuff_id_dict, panoptic_index_dict):
+    pass
+
+convertStuffIDToPanopticIndex(97)
 
 # panopticToIndexPosition(panoptic_category_json)
--- utils.py
+++ utils.py
@@ -3,3 +3,11 @@ import json
 def loadJson(relative_file_location):
     with open(relative_file_location) as json_data:
         return json.load(json_data)
+
+def loadStuffTextLabels(relative_file_location):
+    file = open(relative_file_location, 'r')
+    stuffLabels = {}
+    for line in file:
+        id, name = line.strip().split('\t')
+        stuffLabels[id] = name
+    return stuffLabels
