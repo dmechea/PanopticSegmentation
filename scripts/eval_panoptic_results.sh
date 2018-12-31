a17497418f68ce1f92b5c47cfaa172d14d94f59e
dmechea <daniel.mechea@gmail.com>
2018-12-31 18:09:37 +0900

    eval script running, able to product PQ metric

--- scripts/eval_panoptic_results.sh
+++ scripts/eval_panoptic_results.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python dependencies/panopticapi/evaluation.py
+python dependencies/panopticapi/evaluation.py \
+  --gt_json_file ./dataset/coco/annotations/panoptic_val2017.json \
+  --pred_json_file ./Results/panopticSegmentationResults.json
