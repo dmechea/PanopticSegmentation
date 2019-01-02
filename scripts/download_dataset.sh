10ce73a5bb61dc13eebd40c35db374b1c90b2126
dmechea <daniel.mechea@gmail.com>
2019-01-02 16:26:32 +0900

    download script created to retrieve dataset from dropbox

---
+++ scripts/download_dataset.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# mkdir dataset
+# curl -L -o coco.zip "https://www.dropbox.com/s/v2gl46mtp93ano9/coco.zip?dl=0"
--- scripts/run_inference_and_zip.sh
+++ scripts/run_inference_and_zip.sh
@@ -17,38 +17,28 @@ echo -e "${LIGHTCYAN}Running Inference For Panoptic Analysis ${NC}"
 sleep 0.5
 
 echo -e "${LIGHTCYAN}Running instance segmentation, using MASK_RCNN ${NC}"
-
 python instanceSegmentation.py
-
 echo -e "${LIGHTCYAN}---Done ${NC}"
-
 sleep 0.5
 
 echo -e "${LIGHTCYAN}Running semantic segmentation, using deeplab-V2 ${NC}"
-
 python semanticSegmentation.py
-
 echo -e "${LIGHTCYAN}----Done ${NC}"
-
 sleep 0.5
 
 echo -e "${LIGHTCYAN}Creating images json to store panoptic results... ${NC}"
-
 python createPanopticResultsImageFile.py
-
 echo -e "${LIGHTCYAN}----Done ${NC}"
-
 sleep 0.5
 
 echo -e "${LIGHTCYAN}Combining Segmentations to produce panoptic results ${NC}"
-
 python panopticSegmentation.py
-
 echo -e "${LIGHTCYAN}----Done ${NC}"
-
 sleep 0.5
 
 echo -e "${LIGHTCYAN}Creating zip file for competition submission.... ${NC}"
-
+sleep 0.5
 echo -e "${LIGHTCYAN}Zipfile Name = ${ZIP_NAME}${NC}"
+sleep 0.5
 cd PanopticResults && zip -r "../${ZIP_NAME}.zip" *
+echo -e "${LIGHTCYAN}----Done ${NC}"
