ea5b17af80e39dfa545d2c21cccbe5a7bfd1136a
dmechea <daniel.mechea@gmail.com>
2019-01-02 19:33:01 +0900

    removed gitignore from deeplab folder which was causing issues

---
+++ dependencies/deeplab-pytorch/libs/utils/__init__.py
@@ -0,0 +1,3 @@
+from .loss import CrossEntropyLoss2d
+from .metric import scores
+from .crf import dense_crf
--- scripts/setup_deeplab.sh
+++ scripts/setup_deeplab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# cd dependencies/deeplab-pytorch && ./scripts/setup_caffemodels.sh
+cd dependencies/deeplab-pytorch && ./scripts/setup_caffemodels.sh
 
 cd dependencies/deeplab-pytorch &&
 python convert.py --dataset coco_init
