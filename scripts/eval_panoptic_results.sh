8d45016705a5ba440570cc24677ab661d9e8bde5
dmechea <daniel.mechea@gmail.com>
2019-01-02 10:15:47 +0900

    change data location for eval script

--- scripts/eval_panoptic_results.sh
+++ scripts/eval_panoptic_results.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 python dependencies/panopticapi/evaluation.py \
-  --gt_json_file ./dataset/coco/annotations/panoptic_val2017.json \
+  --gt_json_file ./dataset/coco/val2017/annotations/panoptic_val2017.json \
   --pred_json_file ./Results/panopticSegmentationResults.json
